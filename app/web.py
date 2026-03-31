4207bed42e4e2e449a9983fe2b82bcbf7b632cf6
Christopher Engelbart <cengelbart39@protonmail.com>
2026-03-31 13:11:25 -0400

    List endpoints on GUI

--- app/web.py
+++ app/web.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+from app.database import get_all_endpoints
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    endpoints = get_all_endpoints()
+
+    if endpoints is None:
+        endpoints = []
+
+    return render_template("index.html", endpoints=endpoints)
