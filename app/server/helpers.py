9ba4cf341dc9117b50e7aee32ceecde75170ce91
Christopher Engelbart <cengelbart39@protonmail.com>
2026-04-21 15:55:29 -0400

    Fix time zone conversion

--- app/server/helpers.py
+++ app/server/helpers.py
@@ -1,12 +1,12 @@
-from zoneinfo import ZoneInfo
+import pytz
 
 
 def process_checks(checks):
     processed = []
 
     for check in checks:
-        date_utc = check["checked_at"]
-        date_et = date_utc.astimezone(ZoneInfo("America/New_York"))
+        date_utc = check["checked_at"].replace(tzinfo=pytz.utc)
+        date_et = date_utc.astimezone(pytz.timezone("America/New_York"))
         formatted = date_et.strftime("%B %d, %Y at %I:%M %p")
 
         processed.append(
@@ -25,5 +25,6 @@ def process_checks(checks):
 
 
 def format_timestamp(timestamp):
-    date_et = timestamp.astimezone(ZoneInfo("America/New_York"))
+    timestamp = timestamp.replace(tzinfo=pytz.utc)
+    date_et = timestamp.astimezone(pytz.timezone("America/New_York"))
     return date_et.strftime("%B %d, %Y at %I:%M %p")
